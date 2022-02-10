1d22c77ac4a5d4a9317e62c9b920de28b9e009d9
iq3cloud <github@iq3cloud.com>
2022-02-10 11:32:08 +0000

    Committed by pipeline

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 # A Terraform module to create a subset of cloud components
-# Copyright (C) 2020 IQ3 CLOUD Solutions Direkt GmbH
+# Copyright (C) 2022 IQ3 CLOUD Skaylink GmbH
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
